6d2210128c5bc3a6cd584fdc875b9b0ecd7b676d
Junhee-lee <sk5514224@gmail.com>
2025-05-01 15:28:04 +0900

    feat: Application 클래스 추가 및 의존성 주입 설정, EntryPoint 설정 및 Navigating 틀만 설정

--- app/src/main/java/unithon/helpjob/HelpJobActivity.kt
+++ app/src/main/java/unithon/helpjob/HelpJobActivity.kt
@@ -11,9 +11,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import unithon.helpjob.ui.theme.HelpJobTheme
 
-class MainActivity : ComponentActivity() {
+@AndroidEntryPoint
+class HelpJobActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -36,12 +38,4 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HelpJobTheme {
-        Greeting("Android")
-    }
 }
