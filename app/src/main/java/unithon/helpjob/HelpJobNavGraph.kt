4b247d21c481d18c180743d92936424a3338047e
Junhee-lee <sk5514224@gmail.com>
2025-05-13 16:45:37 +0900

    feat: 인증 관련 네비게이션 초기 구현

--- app/src/main/java/unithon/helpjob/HelpJobNavGraph.kt
+++ app/src/main/java/unithon/helpjob/HelpJobNavGraph.kt
@@ -1,2 +1,36 @@
 package unithon.helpjob
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import unithon.helpjob.ui.auth.signin.SignInScreen
+
+@Composable
+fun HelpJobNavGraph(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
+    startDestination: String = HelpJobDestinations.SIGN_IN_ROUTE
+) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+        modifier = modifier
+    ) {
+        // 로그인 화면
+        composable(route = HelpJobDestinations.SIGN_IN_ROUTE) {
+            SignInScreen(
+                onNavigateToSignUp = {
+                    // TODO: 회원가입 화면으로 이동
+                },
+                onNavigateToMain = {
+                    // TODO: 메인 화면으로 이동
+                }
+            )
+        }
+
+        // TODO: 다른 화면들 추가
+    }
+}
