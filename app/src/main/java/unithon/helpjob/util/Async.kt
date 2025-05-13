ecbae07845c6ffa24c51e2fd35b3c5e13f7a3f85
Junhee-lee <sk5514224@gmail.com>
2025-05-13 14:24:21 +0900

    refactor: Async 래퍼 제거 및 상태 관리 단순화

--- app/src/main/java/unithon/helpjob/util/Async.kt
+++
@@ -1,9 +0,0 @@
-package unithon.helpjob.util
-
-sealed class Async<out T> {
-    object Loading : Async<Nothing>()
-
-    data class Error(val errorMessage: Int) : Async<Nothing>()
-
-    data class Success<out T>(val data: T) : Async<T>()
-}
