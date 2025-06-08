32f4fd3be5e235c1420ea7f30e457f5723208bfa
ACH1002 <103422938+ACH1002@users.noreply.github.com>
2025-06-08 21:02:23 +0900

    [feat] 테스트 의존성 추가
    apk파일 만들기 위해서 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -125,4 +125,9 @@ dependencies {
 
     debugImplementation(composeBom)
     debugImplementation(libs.androidx.compose.ui.tooling.core)
+
+    // 테스트 의존성 추가
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
