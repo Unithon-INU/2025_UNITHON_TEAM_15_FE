3bd6cdc38a042339706d6e8654a8f56a01be9c2f
Junhee-lee <sk5514224@gmail.com>
2025-08-19 14:49:17 +0900

    refactor: build.gradle의 implementation 라이브러리 3개 libs.versions 참조로 교체

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -129,7 +129,7 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling.core)
 
     // 테스트 의존성 추가
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
 }
