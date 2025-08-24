8099cb77a74822a4fc3c166c5e51f1118cbaef5b
Junhee-lee <sk5514224@gmail.com>
2025-08-24 22:56:10 +0900

    refactor: 코루틴 단에서 예외적인 크래시 방지만을 위해 남겨두고 ui로 에러를 보여주니 emit이나 sharedflow는 제거함

--- app/src/main/java/unithon/helpjob/ui/base/BaseViewModel.kt
+++ app/src/main/java/unithon/helpjob/ui/base/BaseViewModel.kt
@@ -2,27 +2,11 @@ package unithon.helpjob.ui.base
 
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.CoroutineExceptionHandler
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.SharedFlow
-import kotlinx.coroutines.flow.asSharedFlow
 import timber.log.Timber
-import unithon.helpjob.util.ErrorHandler
 
 abstract class BaseViewModel : ViewModel() {
-
-    // 에러 이벤트를 UI로 전달
-    private val _errorEvent = MutableSharedFlow<Int>()
-    val errorEvent: SharedFlow<Int> = _errorEvent.asSharedFlow()
-
-    // 공통 CoroutineExceptionHandler
-    protected val errorHandler = CoroutineExceptionHandler { _, throwable ->
-        Timber.e(throwable, "Coroutine exception occurred")
-        val errorMessageResId = ErrorHandler.getErrorMessage(throwable)
-        _errorEvent.tryEmit(errorMessageResId)
-    }
-
-    // 에러 발생 시 UI에 알림
-    protected fun emitError(messageResId: Int) {
-        _errorEvent.tryEmit(messageResId)
+    // 크래시 방지용 (최후의 방어선)
+    protected val crashPreventionHandler = CoroutineExceptionHandler { _, throwable ->
+        Timber.e(throwable, "Unhandled exception - this should have been caught!")
     }
 }
