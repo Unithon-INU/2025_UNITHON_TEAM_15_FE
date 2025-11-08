7d05e2f3f73e5a9f04fbf3f8740dfa79ef1b8b2d
Junhee-lee <sk5514224@gmail.com>
2025-11-08 16:14:09 +0900

    build(deps): Ktor Auth 플러그인 의존성 추가

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -72,6 +72,7 @@ kotlin {
                 implementation(libs.ktor.client.content.negotiation)
                 implementation(libs.ktor.serialization.kotlinx.json)
                 implementation(libs.ktor.client.logging)
+                implementation(libs.ktor.client.auth)
 
                 // DataStore
                 implementation(libs.androidx.dataStore.core)
