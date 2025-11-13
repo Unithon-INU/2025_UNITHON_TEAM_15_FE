8817d270914de251286f3f328adc17acc7e883fb
Junhee-lee <sk5514224@gmail.com>
2025-11-13 13:33:13 +0900

    chore: 레거시 app 모듈 제거

--- app/src/main/java/unithon/helpjob/data/analytics/AnalyticsService.kt
+++
@@ -1,5 +0,0 @@
-package unithon.helpjob.data.analytics
-
-interface AnalyticsService {
-    fun logEvent(name: String, params: Map<String, Any>? = null)
-}
--- app/src/main/java/unithon/helpjob/data/repository/DocumentRepository.kt
+++
@@ -1,9 +0,0 @@
-package unithon.helpjob.data.repository
-
-import unithon.helpjob.data.model.request.DocumentRequest
-
-interface DocumentRepository {
-    suspend fun postCertification(
-        documentRequest: DocumentRequest
-    )
-}
